8db8de448a1f16fdbf6c010cc95608c628b405b8
Roy <sandovalcondori.ra@gmail.com>
2024-11-18 00:44:01 -0500

    Comit final proyecto terminado

--- src/Conversion.java
+++ src/Conversion.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public record Conversion() {
+public record Conversion(
+        String base_code,
+        String target_code,
+        double conversion_rate,
+        double conversion_result
+) {
 }
